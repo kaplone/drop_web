8554adc38833ada5d4e15d29aa90d9339868cfac
kaplone <dvd.brtrnd@gmail.com>
2016-01-27 16:38:17 +0100

    ajout sortie des deux versions (SD + HD)

--- DragAndDropWeb/Drag_and_drop_web/src/web/Convertion.java
+++ DragAndDropWeb/Drag_and_drop_web/src/web/Convertion.java
@@ -48,6 +48,36 @@ public class Convertion {
 		faststart(path.getParent().resolve(path.getFileName().toString().split(".mp")[0] + "_version_web_temp.mp4").toString(),
 				  path.getParent().resolve(path.getFileName().toString().split(".mp")[0] + "_version_web.mp4").toString());
 		
+		String commande2_hd = "C:\\drop_web\\mencoder.exe \"%s\" -ovc x264 -vf scale=1280:720 -sws 9 -x264encopts nocabac:level_idc=30:bframes=0:bitrate=6000:threads=auto:turbo=1:global_header:threads=auto:subq=5:frameref=6:partitions=all:trellis=1:chroma_me:me=umh -oac faac -faacopts mpeg=4:object=2:raw:br=384 -of lavf -lavfopts format=mp4 -o \"%s\"";
+		String commande3_hd = String.format(commande2_hd, path, path.getParent().resolve(path.getFileName().toString().split(".mp")[0] + "_version_web_hd_temp.mp4" ));
+
+		//System.out.println(commande3);
+		
+        Process p_hd = null;
+		
+		try {
+			p_hd = Runtime.getRuntime().exec(commande3_hd);
+			BufferedReader output_hd = getOutput(p_hd);
+            BufferedReader error_hd = getError(p_hd);
+            String ligne_hd = "";
+
+            while ((ligne_hd = output_hd.readLine()) != null) {
+                System.out.println("- " + ligne_hd);
+            }
+            
+            while ((ligne_hd = error_hd.readLine()) != null) {
+                System.out.println("+ " + ligne_hd);
+            }
+            
+			p_hd.waitFor();
+		} catch (IOException | InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		faststart(path.getParent().resolve(path.getFileName().toString().split(".mp")[0] + "_version_web_hd_temp.mp4").toString(),
+				  path.getParent().resolve(path.getFileName().toString().split(".mp")[0] + "_version_web_hd.mp4").toString());
+		
 	}
 	
 	static void faststart(String pathIn, String pathOut) {
